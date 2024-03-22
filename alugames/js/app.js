e774f1b4fd8b0c0854fa569a7e05c282f196fbe8
Aislan Siqueira de Lara <aislan.lara@printer.local>
2024-03-22 17:02:22 -0300

    Atualização

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,4 +1,14 @@
 function alterarStatus(num) {
     let status = document.getElementById(`game-${num}`)
+    let a = status.querySelector("a");
     console.log(status);
+
+    if( a.classList.contains('dashboard__item__button')) {
+        
+        //a.classList.remove('dashboard__item__button');
+        a.classList.add('dashboard__item__button--return');
+    }
+
+    //console.log(status);
+
 }
