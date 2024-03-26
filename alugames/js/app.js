ceffca674e903914b824e992e732a241d2b8d50d
Aislan Siqueira de Lara <aislan.lara@printer.local>
2024-03-26 16:58:48 -0300

    alteração na função

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,14 +1,17 @@
 function alterarStatus(num) {
     let status = document.getElementById(`game-${num}`)
-    let a = status.querySelector("a");
+    let botao = status.querySelector(".dashboard__item__button");
+    let img = status.querySelector(".dashboard__item__img");
 
-    if( a.classList.contains("dashboard__item__button--return")) {
-        a.classList.remove("dashboard__item__button--return");
-        a.textContent = "Alugar";
+    if( botao.classList.contains("dashboard__item__button--return") ) {
+        img.classList.remove("dashboard__item__img--rented");
+        botao.classList.remove("dashboard__item__button--return");
+        botao.textContent = "Alugar";
 
     }else{
-        a.classList.add("dashboard__item__button--return");
-        a.textContent = "Devolver";
+        img.classList.add("dashboard__item__img--rented");
+        botao.classList.add("dashboard__item__button--return");
+        botao.textContent = "Devolver";
     }
 
     
