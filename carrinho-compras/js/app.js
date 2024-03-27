f6de9895253c391c0b2afa4f095cb3749b4c8f42
Aislan Siqueira de Lara <aislan.lara@printer.local>
2024-03-27 14:31:20 -0300

    Atualização

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -5,7 +5,8 @@ function adicionar() {
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1];
 
-    let preco = quantidade * valorUnitario
+    let preco = quantidade * valorUnitario;
+    
     alert(nomeProduto);
     alert(valorUnitario);
     alert(preco);
