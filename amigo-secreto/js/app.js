1d6a1ef1f576224cc8ad80547fa79d1e34e67632
Aislan Siqueira de Lara <aislan.lara@printer.local>
2024-04-12 13:05:24 -0300

    Fim

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -3,6 +3,17 @@ let amigos = [];
 
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
+    if (amigo.value =='' ){
+        alert('Informe o nome do amigo!');
+        return;
+    }
+
+    if (amigos.includes(amigo.value)) {
+        alert('Nome já adicionado!');
+        return;
+    }
+
+
     let lista = document.getElementById('lista-amigos');
     amigos.push(amigo.value);
 
@@ -15,6 +26,10 @@ function adicionar() {
 }
 
 function sortear() {
+    if (amigos.length <= 4){
+        alert('Necessário mínimo 4 pessoas');
+        return;
+    }
     embaralha(amigos);
     let sorteio = document.getElementById('lista-sorteio');
 
@@ -39,3 +54,15 @@ function embaralha(lista) {
     }
 }
 
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+}
+
+for (let numero = 1; numero <= 100; numero++) {
+    if (numero % 5 == 0) {
+        console.log(numero);
+    }
+}
